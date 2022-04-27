e61f9cf55e326799dd605e03e2f779cf36eacc0f
Pierre <88279582+yvpierre@users.noreply.github.com>
2022-04-27 14:59:29 +0200

    vertical text slider on home page

--- src/script.js
+++ src/script.js
@@ -1,7 +1,15 @@
-var loader = document.getElementById("preloaderdefou");
+let loader = document.getElementById("preloaderdefou");
 const cursorRounded = document.querySelector(".rounded");
-var menuItem = document.querySelector(".menu");
-var nom = document.querySelector(".nom");
+let menuItem = document.querySelector(".menu");
+let nom = document.querySelector(".nom");
+const likes = ["True detective", "Museums", "UI Design", "House music", "NBA", "Clothing", "Road trips", "UX Design", "Scorsese's movies"]
+let bothItems = document.querySelector(".item--anim")
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    if(document.getElementsByClassName(''.length > 0)){
+        scrollText();
+    }
+})
 
 menuItem.addEventListener("mouseover", function(){
    menuItem.style.color = "blue !important";
@@ -9,12 +17,18 @@ menuItem.addEventListener("mouseover", function(){
 
 window.addEventListener("load", function(){
     window.setTimeout(
-        removeLoader, 1000
+        function(){
+            removeLoader();
+            var tl = gsap.timeline();
+            tl.to(".nom",{ y:0, opacity:1, duration:1})
+        }, 1000
     );
+
 })
 
 function removeLoader() {
     loader.style.display = "none";
+
 }
 
 const moveCursor = (e)=> {
@@ -25,3 +39,33 @@ const moveCursor = (e)=> {
 
 }
 window.addEventListener('mousemove', moveCursor);
+
+function scrollText() {
+    let item1 = document.querySelector(".item--1")
+    let item2 = document.querySelector(".item--2")
+
+    let wrapper = document.querySelector(".wrapper")
+
+    let interval = setInterval(changeText, 2000);
+
+    function getRandomInt(max, last) {
+        return Math.floor(Math.random() * max);
+    }
+
+    function changeText(){
+        wrapper.classList.add('top')
+        setTimeout(function(){
+            let text1 = item2.textContent
+            let text2 = likes[getRandomInt(likes.length, text1)]
+            item1.innerHTML = text1
+            item2.innerHTML = text2
+            wrapper.classList.remove('top')
+
+        },1000)
+
+
+    }
+
+
+}
+
